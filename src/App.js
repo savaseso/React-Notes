417056429582a8bcfb15e046a4b7893b5842cec6
savaseso <rigbence@gmail.com>
2019-02-28 16:56:10 +0100

    Fetching data from an API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./components/Form"
+import ApiCallFetch from "./components/ApiCallFetch"
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
          <Form />
+         <ApiCallFetch />
          
       </div>
     );
---
+++ src/components/ApiCallFetch.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class ApiCallFetch extends Component {
+    constructor(){
+        super();
+        this.state={
+            loading: false,
+            character:{}
+        }
+    }
+    
+  componentDidMount(){
+    this.setState({loading:true})
+    fetch('https://swapi.co/api/people/4/')
+    .then((res)=>res.json())
+    .then((data)=>{
+      this.setState(
+        { loading:false,
+          character:data
+      })
+    })
+}
+    render(){
+        return(
+            <div>
+                <h1>{this.state.loading?"Loading...":this.state.character.name}</h1>
+            </div>
+        )
+    }
+}
+export default ApiCallFetch
