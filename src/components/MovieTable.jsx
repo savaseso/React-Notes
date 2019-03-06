ff71efddd9349c95b7e5eb35740515f857017a2c
savaseso <rigbence@gmail.com>
2019-03-06 22:49:26 +0100

    like functionality added

--- src/components/MovieTable.jsx
+++ src/components/MovieTable.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import {getMovies} from '../services/fakeMovieService';
+import Like from './common/like';
+
 
 export class MovieTable extends Component {
     state = {
@@ -11,6 +13,14 @@ yű
         const movies = this.state.movies.filter(m => m._id !== movie._id);
         this.setState({movies:movies})
         };
+
+    handleLike = movie => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index] };
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies });
+      }
 
 
   render() {
@@ -29,6 +39,7 @@ yű
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -39,6 +50,7 @@ yű
                       <td>{movie.genre.name}</td>
                       <td>{movie.numberInStock}</td>
                       <td>{movie.dailyRentalRate}</td>
+                      <td><Like liked={movie.liked} onClick={()=>this.handleLike(movie)} /></td>
                       <td><button className="btn btn-danger btn-sm"onClick={() => this.handleDelete(movie)}>Delete</button></td>
                     </tr>)
                   })}
